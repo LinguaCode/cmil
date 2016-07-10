c4fdbb5b8c61a5b05a57a86989785afb6b76e5fd
otanim <otanim617@gmail.com>
2016-07-11 02:26:24 +0400

    code climate 2

--- src/core/formatter/index.js
+++ src/core/formatter/index.js
@@ -9,18 +9,18 @@ exports.parser = function (sessionId, sourceCode, db, isCondition) {
   let scopes = ['\'', '"', '«', '»'];
   let replaceObject = database.languages[db](sessionId, isCondition).replace;
   for (let i = 0; i < replaceObject.length; i++) { //languages
-    let isGlobal = db == 'global';
+    let isGlobal = db === 'global';
     re = new RegExp(isGlobal ? '[^\\\\](' + replaceObject[i].command + ')' : replaceObject[i].command, 'g');
     while ((reStr = re.exec(sourceCode)) !== null) { //in line
       correctResult = reStr[1] ? reStr[1] : reStr[0];
       let indexOfResult = isGlobal ? reStr.index + 1 : reStr.index;
       if (tools.isPartOfCode(sourceCode, indexOfResult)) {
         toReplace = replaceObject[i].definition.replace('$1', correctResult);
-        sourceCode = sourceCode.substring(0, indexOfResult) + toReplace + sourceCode.substring(reStr.index + reStr[0].length);
-      } else if (scopes.indexOf(correctResult) != -1) {
+      } else if (scopes.indexOf(correctResult) !== -1) {
         toReplace = '\\' + correctResult;
-        sourceCode = sourceCode.substring(0, indexOfResult) + toReplace + sourceCode.substring(reStr.index + reStr[0].length);
       }
+      
+      sourceCode = sourceCode.substring(0, indexOfResult) + toReplace + sourceCode.substring(reStr.index + reStr[0].length);
     }
   }
   return sourceCode;
