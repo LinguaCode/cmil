2ebd6491d7ee2a24a4a3914771d8e7310110a0b2
Arman Yeghiazaryan <otanim617@gmail.com>
2017-04-27 20:16:50 +0400

    http2

--- src/server.js
+++ src/server.js
@@ -1,72 +1,83 @@
 const fs = require('fs');
 const io = require('./io');
-const https = require('https');
+const spdy = require('spdy');
 const express = require('express');
 const logger = require('lingua-logger');
-const ENVIRONMENT = require('linguacode-constants').ENVIRONMENT;
+const Router = require('express').Router;
+const path = require('path');
 
-let app = express();
+const router = new Router();
+
+const app = express();
 
 const port = process.env.PORT = process.env.PORT || '3005';
-const environment = process.env.NODE_ENV || 'local';
 app.set('port', port);
 
 require('./routes');
 
-let server;
-
 const certPath = process.env.CERT_FILE_PATH = process.env.CERT_FILE_PATH || './src/config/keys/';
 const privateKeyFilePath = `${certPath}private.key`;
 
-let isCertFilesExist;
 try {
   fs.accessSync(privateKeyFilePath, fs.F_OK);
-  isCertFilesExist = true;
 } catch (e) {
-  isCertFilesExist = false;
+  console.error('SSL certificate file not found.');
+  console.error('Generate a openSSL certificate or use Third party certificate.');
+  return;
 }
 
-if (environment == ENVIRONMENT.PRODUCTION && isCertFilesExist) {
-  const privateKey = fs.readFileSync(`${certPath}private.key`, 'utf8');
-  const certificate = fs.readFileSync(`${certPath}certificate.crt`, 'utf8');
+const privateKey = fs.readFileSync(`${certPath}private.key`, 'utf8');
+const certificate = fs.readFileSync(`${certPath}certificate.crt`, 'utf8');
 
-  let certificates = (function () {
-    let i, len, results;
-    results = [];
-    let certFileList = [
-      'ca_bundle.crt',
-      'certificate.crt',
-    ];
-    for (i = 0, len = certFileList.length; i < len; i++) {
-      let file = certFileList[i];
-      results.push(fs.readFileSync(certPath + file, 'utf8'));
-    }
-    return results;
-  })();
+let certificates = (function () {
+  const results = [];
+  const certFileList = [
+    'ca_bundle.crt',
+    'certificate.crt',
+  ];
+  for (let i = 0, len = certFileList.length; i < len; i++) {
+    const certFile = certFileList[i];
+    results.push(fs.readFileSync(path.join(certPath, certFile), 'utf8'));
+  }
+  return results;
+})();
 
-  let credentials = {
-    ca: certificates,
-    key: privateKey,
-    cert: certificate
-  };
+const credentials = {
+  ca: certificates,
+  key: privateKey,
+  cert: certificate
+};
 
-  server = https.createServer(credentials, app);
+const server = spdy.createServer(credentials, app);
 
-  server.listen(port, function () {
-    console.log(`Server: https://localhost:${port} is listen.`);
-  });
-} else {
-  let http = require('http');
-  server = http.createServer(app);
-
-  server.listen(port, function () {
-    console.llog(`Server: http://localhost:${port} is listen.`);
-  });
-}
+server.listen(port, () => {
+  console.log(`Server: https://localhost:${port} is listen.`);
+});
 
 io.attach(server);
 
 server.on('listening', onListening);
+
+
+app.get('*', (req, res, next) => {
+  res.status(200).send(`<pre>  
+  Core is running...
+ 
+  ┌─────┐   ┌─────┐
+  │     │   │     │
+  │     │   │     │
+  │     │   └─────┘
+  │     │
+  │     └─────────┐ 
+  │               │ 
+  │               │
+  └───────────────┘  
+     
+    LinguaCode.me
+ 
+  </pre>
+`);
+});
 
 function onListening() {
   const address = server.address();
