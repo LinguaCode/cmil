ffcbcfd430a0a1f651aebc2d0b189498288e7403
Arman Yeghiazaryan <otanim617@gmail.com>
2017-05-19 12:08:59 +0400

    update dependencies

--- test/collection/testDB.js
+++ test/collection/testDB.js
@@ -2,7 +2,7 @@ module.exports = [{
   group: 'restrictions',
   sources: [{
     title: 'hack attempt',
-    code: 'b = քառակուսի(2)*քառակուսի(3)\nելք(b)',
-    output: '36'
+    code: 'եթե 4 > 2\n    ելք(«4 > 2»)\nայլապես եթե 4 > 6\n    ելք(«4 > 6»)\nայլապես\n    ելք(«4 == 4»)',
+    output: '4 > 2'
   }]
 }];
--- test/sockets.js
+++ test/sockets.js
@@ -156,8 +156,8 @@ describe('initialize', () => {
 });
 
 /**passed db test*/
-//const dbs = ['successes', 'tutorials', 'errors'];
-const dbs = ['testDB'];
+const dbs = ['successes', 'tutorials', 'errors'];
+// const dbs = ['testDB'];
 dbs.forEach((db) => {
   describe(db, () => {
     const sources = require(`./collection/${db}`);
