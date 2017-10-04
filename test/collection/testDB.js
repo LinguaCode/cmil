4b57094aee008d85b0e9126c8567fe700d3c0929
Arman Yeghiazaryan <otanim617@gmail.com>
2017-10-04 23:14:56 +0300

    function scope bug fix

--- test/collection/testDB.js
+++ test/collection/testDB.js
@@ -2,7 +2,7 @@ module.exports = [{
   group: 'restrictions',
   sources: [{
     title: 'if-else: wrong answer',
-    code: 'X = 2\nY = 64\nZ = log(X,Y)\noutput Z',
+    code: 'input x\ninput y\nA=(x+y)*(x+power((square(x)+1),6)*sin(square(x)-3)-tan(y))\noutput A',
     inputs: [1, 2],
     output: '8.88440488015187'
   }]
